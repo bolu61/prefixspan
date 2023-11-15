d14194e2f086725ae2f122b50639d6c9ac8cc80b
bolu <bolu@bolupi.me>
2023-11-15 17:02:49 -0500

    remove bindings to design algorithm

--- bindings/python/bindings.cxx
+++ bindings/python/bindings.cxx
@@ -1,8 +1,8 @@
-#include <nanobind/nanobind.h>
-#include <nanobind/stl/bind_vector.h>
 #include "prefixspan.hxx"
 
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/bind_vector.h>
+
 NB_MODULE(prefixspan, m) {
-    nanobind::bind_vector<std::vector<unsigned int>>(m, "int_vector");
-    m.def("build", &prefixspan::build<std::vector<unsigned int>>);
+	nanobind::bind_vector<std::vector<unsigned int>>(m, "int_vector");
 }
