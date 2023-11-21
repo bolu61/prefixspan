c7020c691868478fd30bbb046109caabc7f6e2d2
bolu <bolu@bolupi.me>
2023-11-21 18:32:04 -0500

    first prototype implementation

--- prefixspan/bindings/python/src/bindings.cxx
+++ prefixspan/bindings/python/src/bindings.cxx
@@ -1,20 +1,70 @@
 #include "prefixspan.hxx"
+#include "trie.hxx"
 
 #include <cstddef>
 #include <nanobind/nanobind.h>
-#include <nanobind/stl/bind_vector.h>
-#include <ranges>
+#include <nanobind/stl/vector.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/make_iterator.h>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
 
-NB_MODULE(prefixspan, m) {
+namespace nb = nanobind;
+namespace ps = prefixspan;
+
   using data_t = std::size_t;
   using sequence_t = std::vector<data_t>;
   using database_t = std::vector<sequence_t>;
 
-  nanobind::bind_vector<sequence_t>(m, "sequence");
-  nanobind::bind_vector<database_t>(m, "database");
 
-  m.def("make", [](database_t const & database, std::size_t const & minsup) {
-    return prefixspan::make<database_t, data_t>(database, minsup);
-  });
+std::string stringify(prefixspan::trie<data_t> const & t) {
+  if (t.empty()) {
+    return "";
+  }
+  std::vector<std::string> strings;
+  for (auto const & [symbol, next] : t.unfix()) {
+    strings.emplace_back(std::to_string(symbol) + ',' + stringify(next) + ";");
+  }
+  std::ostringstream out;
+  std::copy(
+    strings.begin(),
+    strings.end(),
+    std::ostream_iterator<std::string>(out, "")
+  );
+  return out.str();
+}
+
+NB_MODULE(prefixspan, m) {
+  nanobind::class_<prefixspan::trie<data_t>>(m, "trie")
+    .def(
+      "__getitem__",
+      [](prefixspan::trie<data_t> const & t, data_t const & key) {
+        return t.at(key);
+      }
+    )
+    .def("empty", &prefixspan::trie<data_t>::empty)
+    .def(
+      "__iter__",
+      [](prefixspan::trie<data_t> const & t) {
+        auto iterator = t.unfix();
+        return nanobind::make_iterator(
+          nanobind::type<prefixspan::trie<data_t>>(),
+          "iterator",
+          iterator.begin(),
+          iterator.end()
+        );
+      },
+      nanobind::keep_alive<0, 1>()
+    )
+    .def("__repr__", &stringify);
+
+  m.def(
+    "make",
+    [](
+      database_t const & db,
+      std::size_t const & minsup
+    ) { return prefixspan::make<data_t, database_t>(db, minsup); }
+  );
 }
