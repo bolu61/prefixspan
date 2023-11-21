26bf78043686f715b0d07bf127b9613050dd1e42
bolu61 <bolu61@zjc.dev>
2023-11-21 18:32:04 -0500

    first prototype implementation

--- prefixspan/src/prefixspan.hxx
+++ prefixspan/src/prefixspan.hxx
@@ -2,24 +2,46 @@
 
 #include "./trie.hxx"
 
+#include <iostream>
 #include <map>
 #include <ranges>
+#include <vector>
 
 namespace prefixspan {
-  // todo: implement function of projected database
+  namespace core {
+    template<typename range_t>
+    concept range = std::ranges::random_access_range<range_t> &&
+      std::ranges::sized_range<range_t>;
 
-  template<typename database_t, typename symbol_t>
-  requires std::ranges::range<database_t> &&
-    std::ranges::range<std::ranges::range_value_t<database_t>> &&
-    std::same_as<
-             std::ranges::range_value_t<std::ranges::range_value_t<database_t>>,
-             symbol_t>
-  trie<symbol_t> make(database_t const & database, std::size_t const & minsup) {
-    // todo @bolu61
+    template<typename range_t>
+    using value_t = std::ranges::range_value_t<range_t>;
+
+    template<typename range_t, typename symbol_t>
+    concept database = range<range_t> && range<value_t<range_t>> &&
+      std::same_as<value_t<value_t<range_t>>, symbol_t>;
+
+    template<typename symbol_t, database<symbol_t> database_t>
+    auto project(database_t const & db, symbol_t const & symbol) {
+      auto transform = std::views::transform([&symbol](auto const & sequence) {
+        auto begin = std::ranges::find(sequence, symbol);
+        if (begin != sequence.end()) {
+          begin = std::ranges::next(begin);
+        }
+        return std::ranges::subrange(begin, sequence.end());
+      });
+      auto db_new = db | transform;
+      return std::vector(db_new.begin(), db_new.end());
+    };
+  }; // namespace core
+
+  template<typename symbol_t, core::database<symbol_t> database_t>
+  trie<symbol_t> make(database_t const & db, std::size_t const & minsup) {
     trie<symbol_t> t;
 
+    auto db_view = db;
+
     std::map<symbol_t, std::size_t> count;
-    for (auto sequence : database) {
+    for (auto sequence : db_view) {
       for (symbol_t symbol : sequence) {
         count[symbol] += 1;
       }
@@ -27,8 +49,8 @@ namespace prefixspan {
 
     for (auto [symbol, c] : count) {
       if (c >= minsup) {
-        // todo: project, recurse, then insert
-        t.insert(symbol);
+        auto db_new = core::project(db_view, symbol);
+        t.insert(symbol, make<symbol_t>(db_new, minsup));
       }
     }
 
--- prefixspan/src/trie.hxx
+++ prefixspan/src/trie.hxx
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <map>
 #include <ranges>
 
@@ -11,9 +13,37 @@ namespace prefixspan {
     auto insert(key_t const & key, trie<key_t> && tries) {
       return this->subtries.emplace(key, std::move(tries));
     };
-    
+
     auto insert(key_t const & key) {
       return this->subtries.emplace(key, std::move(trie<key_t>()));
+    }
+
+    bool contains(key_t const & key) {
+      return this->subtries.contains(key);
+    }
+
+    auto const & unfix() const {
+      return this->subtries;
+    }
+
+    auto & at(key_t const & key) {
+      return this->subtries.at(key);
+    }
+
+    auto const & at(key_t const & key) const {
+      return this->subtries.at(key);
+    }
+
+    auto & operator[](key_t const & key) {
+      return this->at(key);
+    }
+
+    auto const & operator[](key_t const & key) const {
+      return this->at(key);
+    }
+
+    bool empty() const {
+      return this->subtries.empty();
     }
   };
 }; // namespace prefixspan
